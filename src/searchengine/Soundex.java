7500929bb5cce26b7e60726f12393801e83a313f
manikhanuja <mani.khanuja@pb.com>
2016-10-01 14:48:44 -0700

    Updated the Soundex Algo

--- src/searchengine/Soundex.java
+++ src/searchengine/Soundex.java
@@ -8,24 +8,23 @@ package searchengine;
 /**
  *
  * @author manikhanuja
- 
-*/
-public class Soundex { 
-    public static String soundex(String s) { 
-        char[] x = s.toUpperCase().toCharArray();
-        char firstLetter = x[0];
+ *
+ */
+public class Soundex {
 
-        // CONVERT letters to numeric code
-        for (int i = 0; i < x.length; i++) {
-            switch (x[i]) {
+    public static String soundex(String sname) {
+        char[] coder = sname.toUpperCase().toCharArray();
+        char firstLetter = coder[0];
 
+        // conversion of letters in name to numeric code
+        for (int i = 0; i < coder.length; i++) {
+            switch (coder[i]) {
                 case 'B':
                 case 'F':
                 case 'P':
                 case 'V':
-                    x[i] = '1';
+                    coder[i] = '1';
                     break;
-
                 case 'C':
                 case 'G':
                 case 'J':
@@ -34,51 +33,36 @@ public class Soundex {
                 case 'S':
                 case 'X':
                 case 'Z':
-                    x[i] = '2';
+                    coder[i] = '2';
                     break;
-
                 case 'D':
                 case 'T':
-                    x[i] = '3';
+                    coder[i] = '3';
                     break;
-
                 case 'L':
-                    x[i] = '4';
+                    coder[i] = '4';
                     break;
-
                 case 'M':
                 case 'N':
-                    x[i] = '5';
+                    coder[i] = '5';
                     break;
-
                 case 'R':
-                    x[i] = '6';
+                    coder[i] = '6';
                     break;
-
                 default:
-                    x[i] = '0';
+                    coder[i] = '0';
                     break;
             }
         }
-
-        // REMOVE duplicates
+        // remove any dulipcate items
         String output = "" + firstLetter;
-        for (int i = 1; i < x.length; i++)
-            if (x[i] != x[i-1] && x[i] != '0')
-                output += x[i];
-
-        // pad with 0's or truncate
+        for (int i = 1; i < coder.length; i++) {
+            if (coder[i] != coder[i - 1] && coder[i] != '0') {
+                output += coder[i];
+            }
+        }
+        // padding with 0's or truncate
         output = output + "0000";
         return output.substring(0, 4);
     }
-
-
-    /*public static void main(String[] args) {
-        String name1 = args[0];
-        String name2 = args[1];
-        String code1 = soundex(name1);
-        String code2 = soundex(name2);
-        System.out.println(code1 + ": " + name1);
-        System.out.println(code2 + ": " + name2);
-    }*/
 }
